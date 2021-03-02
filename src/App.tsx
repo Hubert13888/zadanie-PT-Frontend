e6b0d203b850735b43821fd0a4f1430af7c80e42
Hubert13888 <31376199+Hubert13888@users.noreply.github.com>
2021-03-02 20:32:26 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <h3>1</h3>
+      <h4>2</h4>
+      <h5>3</h5>
+      <h6>4</h6>
+    </div>
+  );
+}
